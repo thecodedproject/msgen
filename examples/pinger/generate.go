1dedb52f16fa6990232d1df121a3eaa9a8574669
Jonny Cooper <jcooper@luno.com>
2020-09-27 21:17:36 +0100

    examples: Fix example ms

--- examples/pinger/generate.go
+++ examples/pinger/generate.go
@@ -1,3 +1,5 @@
-package nested_pinger
+package pinger
 
 //go:generate go run ../../main.go --import github.com/thecodedproject/msgen/examples/pinger
+
+//go:generate protoc --go_out=plugins=grpc:. pingerpb/pinger.proto
